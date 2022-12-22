d66f2073a7b0c4459a16ec386e9257372bebdcad
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-12-23 04:27:18 +0800

    feat(controller.register): change status code
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -2,7 +2,7 @@ import { createService } from '#services/users.service.js';
 
 const registerController = async (req, res) => {
   const result = await createService(req.body);
-  return res.status(result.ok ? 204 : 400).json(result);
+  return res.status(result.ok ? 201 : 401).json(result);
 };
 
 export default registerController;
