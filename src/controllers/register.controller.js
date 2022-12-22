52eba670d036fb796a73d54d376b21218f60f5d3
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-12-23 02:51:02 +0800

    lint: format
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -3,7 +3,6 @@ import { createService } from '#services/users.service.js';
 const $registerController = async (req, res) => {
   const result = await createService(req.body);
   return res.status(result.ok ? 204 : 400).json(result);
-}
+};
 
 export default $registerController;
-
